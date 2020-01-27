60b45e9a845f3bfdf9fa2ed7f76a8a4642a31beb
GeniusMarco <rider_112@o2.pl>
2020-01-27 21:55:46 +0100

    initial commit

---
+++ src/test/java/com/gm/cvanishserver/CvanishServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.cvanishserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvanishServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
