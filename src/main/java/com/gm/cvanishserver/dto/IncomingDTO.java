185751b2c0a2156d651bea5028a096310396ffb4
GeniusMarco <rider_112@o2.pl>
2020-02-13 16:05:54 +0100

    refactoring

--- src/main/java/com/gm/cvanishserver/dto/IncomingDTO.java
+++ src/main/java/com/gm/cvanishserver/dto/IncomingDTO.java
@@ -2,12 +2,14 @@ package com.gm.cvanishserver.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 import java.util.Map;
 
 @Builder
 @Data
+@ToString(exclude = {"summary", "skills"})
 public class IncomingDTO {
     private String firstName;
     private String lastName;
